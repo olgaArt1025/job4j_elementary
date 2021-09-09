d3409a03e21a4fbf08b307781f520ea80737e46e
Olga_ <olga_art1025@mail.ru>
2021-09-09 21:35:17 +0500

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/calculate/ArgMethod.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    @SuppressWarnings("checkstyle:WhitespaceAfter")
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 33;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+}
+

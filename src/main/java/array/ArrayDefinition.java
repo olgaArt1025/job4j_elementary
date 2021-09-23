92ea2667ed59022ed3ae24229966a62cbb99ef07
Olga_ <olga_art1025@mail.ru>
2021-09-23 21:00:16 +0500

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/array/ArrayDefinition.java
+++ src/main/java/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Kendall Blue";
+        names[1] = "Cara White";
+        names[2] =  "Bella Black";
+        names[3] = "Anna Green";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
